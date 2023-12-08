0bed611dc7240d7d93848d257929356dc2d6f723
Camila Florez <camila.rodriguez.florez@gmail.com>
2023-12-08 20:00:01 +0100

    Navbar.jsx update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,7 @@ import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 
 function Navbar() {
-  // Subscribe to the AuthContext to gain access to
-  // the values from AuthContext.Provider `value` prop
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext)
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
     <nav>
@@ -15,21 +13,29 @@ function Navbar() {
       <Link to="/concerts">
         <button>Concerts</button>
       </Link>
-
       {isLoggedIn && (
         <>
           <Link to="/concerts/add">
-            <button>Add concert</button>
+            <button>Add Concert</button>
+          </Link>
+          <Link to="/venues">
+            <button>Venues</button>
+          </Link>
+          <Link to="/venues/add">
+            <button>Add Venue</button>
           </Link>
           <button onClick={logOutUser}>Logout</button>
           <span>{user && user.name}</span>
         </>
       )}
-
       {!isLoggedIn && (
         <>
-          <Link to="/signup"><button>Sign Up</button></Link>
-          <Link to="/login"><button>Login</button></Link>
+          <Link to="/signup">
+            <button>Sign Up</button>
+          </Link>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
         </>
       )}
     </nav>
