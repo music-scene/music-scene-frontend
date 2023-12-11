9c63e79a4b2684a4a28b31e650ff6ac3b548e2c2
Camila Florez <camila.rodriguez.florez@gmail.com>
2023-12-11 11:26:15 +0100

    Venue Details page + additional details

--- src/pages/VenueDetailsPage.jsx
+++ src/pages/VenueDetailsPage.jsx
@@ -1,14 +1,12 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
 import venueService from '../services/venue.service';
 
 function VenueDetailsPage() {
-
     const [venueDetails, setVenueDetails] = useState(null);
     const { venueId } = useParams();
 
-     const getVenueById = () => {
+    const getVenueById = () => {
         venueService.getVenueById(venueId)
             .then((response) => {
                 setVenueDetails(response.data);
@@ -21,7 +19,6 @@ function VenueDetailsPage() {
     }, [venueId]);
 
     return (
-
         <div>
             <div>
                 {venueDetails === null
@@ -29,8 +26,11 @@ function VenueDetailsPage() {
                     : (
                         <div>
                             <h1>{venueDetails.name}</h1>
-                            <p>{venueDetails.description}</p>
-
+                            <p><strong>Location:</strong> {venueDetails.location}</p>
+                            <p><strong>Description:</strong> {venueDetails.description}</p>
+                            <p><strong>Capacity:</strong> {venueDetails.capacity}</p>
+                            <img src={venueDetails.imageUrl} alt={venueDetails.name} />
+                            {/* Add more details as needed */}
                         </div>
                     )}
             </div>
