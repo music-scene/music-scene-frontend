d16d0ac0485fb5729110a60bbc103f86e22bfe03
Camila Florez <camila.rodriguez.florez@gmail.com>
2023-12-12 12:41:39 +0100

    break points List pages css

--- src/pages/VenueListPage.jsx
+++ src/pages/VenueListPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import venueService from '../services/venue.service';
-import './VenueListPage.css';
+import './ListPages.css';
 
 function VenueListPage() {
   const [venues, setVenues] = useState(null);
