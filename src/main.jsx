e9bc71349ddddc235422a8c4b88afec6c8ecdb73
Vasco Martins <vasco.martins.drummer@gmail.com>
2023-12-12 13:23:22 +0000

    revert back to no tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from "react-router-dom"
 import { AuthProviderWrapper } from "./context/auth.context"
-import { NextUIProvider } from "@nextui-org/react";
 import App from './App.jsx'
 /* import './index.css' */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NextUIProvider>
       <Router>
         <AuthProviderWrapper>
           <App />
         </AuthProviderWrapper>
       </Router>
-    </NextUIProvider>
   </React.StrictMode>,
 )
