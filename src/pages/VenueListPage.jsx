1a72ed8076b0b1e6a31c690c772ddf74e6af1353
Camila Florez <camila.rodriguez.florez@gmail.com>
2023-12-13 11:26:30 +0100

    add button

--- src/pages/VenueListPage.jsx
+++ src/pages/VenueListPage.jsx
@@ -57,7 +57,15 @@ function VenueListPage() {
 
         {isLoggedIn && (
           <div className="AddButton">
-            <Link to="/venues/add">Add venue</Link>
+            <Link to="/venues/add">
+              <div tabIndex="0" className="plusButton">
+                <svg className="plusIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                  <g mask="url(#mask0_21_345)">
+                    <path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z"></path>
+                  </g>
+                </svg>
+              </div>
+            </Link>
           </div>
         )}
       </div>
