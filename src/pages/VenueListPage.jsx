4fe6d8e936202513ab751610b0aaf277d32732c4
Camila Florez <camila.rodriguez.florez@gmail.com>
2023-12-13 13:01:53 +0100

    Location + Lists responsive breaking points

--- src/pages/VenueListPage.jsx
+++ src/pages/VenueListPage.jsx
@@ -39,54 +39,58 @@ function VenueListPage() {
     }
   }, [searchValue, venues]);
 
-  return (
-    <div className="VenueListPageContainer">
-      <div className="RowContainer">
-        <div className="inputContainer">
-          <form>
-            <input
-              className="inputField"
-              type="text"
-              value={searchValue}
-              onChange={handleSearch}
-              required
-            />
-            <label className="inputLabel">SEARCH</label>
-          </form>
-        </div>
 
-        {isLoggedIn && (
-          <div className="AddButton">
-            <Link to="/venues/add">
-              <div tabIndex="0" className="plusButton">
-                <svg className="plusIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                  <g mask="url(#mask0_21_345)">
-                    <path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z"></path>
-                  </g>
-                </svg>
-              </div>
-            </Link>
-          </div>
-        )}
+return (
+  <div className="VenueListPageContainer">
+    <div className="RowContainer">
+      <div className="inputContainer">
+        <form>
+          <input
+            className="inputField"
+            type="text"
+            value={searchValue}
+            onChange={handleSearch}
+            required
+          />
+          <label className="inputLabel">SEARCH</label>
+        </form>
       </div>
 
-      {!displayVenues && <h1>No venues available</h1>}
-      {displayVenues === null ? (
-        <h1>Venues list is loading...</h1>
-      ) : (
-        <div>
-          {displayVenues.map((venue) => (
-            <div className="VenueContainer" key={venue._id}>
-              <Link to={`/venues/${venue._id}`}>
-                <img src={venue.imageUrl} alt={venue.name} />
-                <h2>{venue.name}</h2>
-              </Link>
+      {isLoggedIn && (
+        <div className="AddButton">
+          <Link to="/venues/add">
+            <div tabIndex="0" className="plusButton">
+              <svg className="plusIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <g mask="url(#mask0_21_345)">
+                  <path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z"></path>
+                </g>
+              </svg>
             </div>
-          ))}
+          </Link>
         </div>
       )}
     </div>
-  );
+
+    {!displayVenues && <h1>No venues available</h1>}
+    {displayVenues === null ? (
+      <h1>Venues list is loading...</h1>
+    ) : (
+      <div>
+        {displayVenues.map((venue) => (
+          <div className="VenueContainer" key={venue._id}>
+            <Link to={`/venues/${venue._id}`}>
+              <img src={venue.imageUrl} alt={venue.name} />
+              <h2>{venue.name}</h2>
+              {/* Add the location with a different class */}
+              <p className="VenueLocation">{venue.location}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+    )}
+  </div>
+);
+
 }
 
 export default VenueListPage;
